da06567e254231fe1fbbfc4712f2ddaa8ad6d123
Yejin Kim <86982496+dpwls8984@users.noreply.github.com>
2026-02-12 22:03:03 +0900

    [level 0] Title: 카운트 업, Time: 0.02 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/0/181920. 카운트 업/카운트 업.java
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.java
@@ -1,13 +1,10 @@
 class Solution {
     public int[] solution(int start_num, int end_num) {
-        int n = end_num-start_num +1;
-        int[] answer = new int[n];
-        //숫자를 리스트에 담는다 -> 어떻게?
-//      return Arrays.stream(numbers).map(i -> i * 2).toArray();
+
+        int[] answer = new int[end_num-start_num+1];        
         
-        for(int i=0;i<n;i++) {
-            answer[i] = start_num;
-            start_num++;
+        for(int i=0;i<answer.length;i++) {
+            answer[i] = start_num+i;
         }
         return answer;
     }
