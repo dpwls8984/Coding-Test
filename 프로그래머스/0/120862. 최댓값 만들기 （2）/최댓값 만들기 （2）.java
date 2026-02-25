8af3b019d4e48784739121ab55b991b175ff5557
Yejin Kim <86982496+dpwls8984@users.noreply.github.com>
2026-02-25 23:55:32 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.19 ms, Memory: 81.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = -500000000;
+        for(int i =0; i<numbers.length;i++){
+            for(int j = i+1; j<numbers.length;j++){
+                int max = numbers[i] * numbers[j];    
+                if(max > answer){
+                    answer = max;   
+                }
+            }
+        }
+        return answer;
+    }
+}
