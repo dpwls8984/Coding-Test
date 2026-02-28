6773493f9ae3bf7223e8ba5f808f7a49c933f735
Yejin Kim <86982496+dpwls8984@users.noreply.github.com>
2026-02-28 21:41:43 +0900

    [level 2] Title: 전화번호 목록, Time: 287.17 ms, Memory: 224 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,11 +2,18 @@ import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        Arrays.sort(phone_book);
+        Map<String, Integer> map = new HashMap<>();
         
-        for(int i=0;i<phone_book.length-1;i++){
-            if(phone_book[i+1].startsWith(phone_book[i]))
-                return false;
+        for(String p: phone_book) map.put(p, 1);
+        
+        for(String p: phone_book) {
+            for(int i=0;i<p.length();i++){
+                String prefix = p.substring(0, i);
+                
+                if(map.containsKey(prefix)){
+                    return false;
+                }
+            }
         }
         
         return true;
