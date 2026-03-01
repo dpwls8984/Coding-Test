0658c776ab3b6e0de9dade4edd5ff62841e5a348
Yejin Kim <86982496+dpwls8984@users.noreply.github.com>
2026-03-01 17:47:56 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 82.43 ms, Memory: 95.8 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -4,20 +4,21 @@ class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
         
-        Map<String, Integer> participantMap = new HashMap<>();
-        
-        for(String p: participant) 
-            participantMap.put(p, participantMap.getOrDefault(p, 0)+1);
+        Map<String, Integer> map = new HashMap<>();
 
-        for(String c: completion)
-            participantMap.put(c, participantMap.get(c)-1);
+        for(String p: participant) 
+            map.put(p, map.getOrDefault(p, 0)+1);
         
-        for(String key: participantMap.keySet()){
-            if(participantMap.get(key)!=0){
-                answer = key;
-                break;
-            } 
+        for(String c: completion){
+            map.put(c, map.get(c)-1);
         }
+        
+        for(String key: map.keySet()){
+            if(map.get(key) != 0){
+                answer = key;
+            }
+        }
+        
         return answer;
     }
 }
