f5abf9d6fbc2f8560e37c73a00d3d203f9266b70
Yejin Kim <86982496+dpwls8984@users.noreply.github.com>
2026-03-15 00:04:29 +0900

    [level 0] Title: 암호 해독, Time: 0.29 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(String cipher, int code) {
+        StringBuffer stringBuffer = new StringBuffer();
+        int a = 1;
+
+        for(int i = 0;i<cipher.length()+1;i++){
+            if(i == (code*a)){
+                stringBuffer.append(cipher.charAt(i-1));
+                a++;
+            }
+        }
+        
+        return stringBuffer.toString();
+    }
+}
