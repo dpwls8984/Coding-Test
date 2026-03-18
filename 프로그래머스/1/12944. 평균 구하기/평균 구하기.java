2680e863fd75a293e25938d31ec47a0e79f4fa52
Yejin Kim <86982496+dpwls8984@users.noreply.github.com>
2026-03-19 00:18:30 +0900

    [level 1] Title: 평균 구하기, Time: 4.27 ms, Memory: 86.8 MB -BaekjoonHub

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -1,9 +1,8 @@
+import java.util.*;
+
 class Solution {
     public double solution(int[] arr) {
-        int sum = 0;
-        for(int n: arr){
-            sum += n;
-        }
-        return (double)sum/(arr.length);
+
+        return Arrays.stream(arr).average().getAsDouble();
     }
 }
