0755e286d0cfffd4c3314811516a911cff0772ed
Yejin Kim <86982496+dpwls8984@users.noreply.github.com>
2026-03-22 00:59:53 +0900

    [level 1] Title: 약수의 합, Time: 17.96 ms, Memory: 86 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.java
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.java
@@ -1,12 +1,11 @@
+import java.util.stream.IntStream;
+
 class Solution {
     public int solution(int n) {
-        int answer = 0;
-        for(int i=1;i<=n;i++){
-            if(n%i==0){
-                answer += i;
-            }
-        }
-        
-        return answer;
+        return IntStream
+            .rangeClosed(1, n)
+            .parallel()
+            .filter(i -> n%i ==0)
+            .sum();
     }
 }
