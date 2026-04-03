985836a37cbe0cab4153f59b62494cc3fa7144db
Yejin Kim <86982496+dpwls8984@users.noreply.github.com>
2026-04-04 00:33:33 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.02 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 45;
+        
+        for(int a: numbers){
+            answer -= a;
+        }        
+        return answer;
+    }
+}
