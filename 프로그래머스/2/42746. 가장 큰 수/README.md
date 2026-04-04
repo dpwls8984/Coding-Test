0e97159e5aba4e856ecaa32c51f750c3d8000651
Yejin Kim <86982496+dpwls8984@users.noreply.github.com>
2026-04-04 19:25:25 +0900

    [level 2] Title: 가장 큰 수, Time: 206.38 ms, Memory: 128 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/README.md
+++ 프로그래머스/2/42746. 가장 큰 수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 가장 큰 수 - 42746 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746) 
 
 ### 성능 요약
 
-메모리: 126 MB, 시간: 187.85 ms
+메모리: 128 MB, 시간: 206.38 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 03월 20일 03:07:54
+2026년 04월 04일 19:25:22
 
 ### 문제 설명
 
--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -2,39 +2,62 @@ import java.util.*;
 
 class Solution {
     public String solution(int[] numbers) {
-        String[] arr = new String[numbers.length];
-        StringBuffer stringBuffer = new StringBuffer();
+        String answer = "";
         
-        for(int i=0;i<numbers.length;i++){
+        String[] arr = new String[numbers.length];
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i = 0; i < numbers.length; i++){
             arr[i] = String.valueOf(numbers[i]);
         }
         
-        Arrays.sort(arr, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
+        Arrays.sort(arr, (a, b) -> (b+a).compareTo(a+b));
+        /* 
+        6, 10, 2
         
+        a: 6, b: 10
+        106.compareTo(610)
+        106<610
+        
+        6, 10
+        
+        a: 10, b: 2
+        210.compareTo(102)
+        210>102 -> 210이 앞에 간다
+        
+        2, 10
+        
+        즉 6, 2, 10
+        a: 6, b: 2
+        26.compareTo(62)
+        26<62
+        
+        6, 2, 10
+        
+        ---
+        Arrays.sort(arr, (a, b) -> (a + b).compareTo(b+a));
+
+        6, 10, 2
+        
+        a: 6, b: 10
+        610.compareTo(106)
+        610 > 106
+        6, 10
+        
+        10, 2
+        a: 10, b: 2
+        102.compareTo(210)
+        102 < 210
+        
+        2, 10
+        
+        */
         if(arr[0].equals("0")){
             return "0";
         }
         
-        for(String a: arr){
-            stringBuffer.append(a);
-        }
+        for(String a: arr) sb.append(a);
         
-        /* 
-        o1: 30, o2: 9
-        o1, o2 -> (o2 + o1).compareTo(o1 + o2)
-        
-        930 > 309 -> (+) ->  -> o1, o2 중에서 o2가 앞에감
-        9, 30
-        
-        
-        o1: 30, o2: 9
-        
-        309.compareTo(930)
-        309 < 930 -> 뒤에 값이 더 크다 (-) -> 앞에 값(o1)부터 쓴다
-        30, 9
-        */
-        
-        
-        return stringBuffer.toString();
+        return sb.toString();
     }
 }
